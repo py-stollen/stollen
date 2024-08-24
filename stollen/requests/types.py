24dec131e8791e53eca3745657c8892d9862e1a1
wakaree <svegliatixanax42@gmail.com>
2024-08-24 23:06:02 +0300

    Fixed query validation

--- stollen/requests/types.py
+++ stollen/requests/types.py
@@ -8,7 +8,7 @@ class StollenRequest(BaseModel):
     http_method: str
     response_data_key: list[str]
     headers: dict[str, Any] = Field(default_factory=dict)
-    query: dict[str, str] = Field(default_factory=dict)
+    query: dict[str, str | int | float] = Field(default_factory=dict)
     body: Optional[Any] = None
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
