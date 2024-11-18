0f973a8160c46c931b64819bce24761784e7510d
wakaree <svegliatixanax42@gmail.com>
2024-11-18 18:38:42 +0200

    Fix request and response serialization when displaying detailed API error

--- stollen/utils/text.py
+++ stollen/utils/text.py
@@ -1,3 +1,17 @@
+import json
+
+from pydantic import BaseModel
+
+
 def camel_to_snake(string: str) -> str:
     parts: list[str] = [f"{c.lower()}_" if c.isupper() else c for c in string]
     return "".join(parts).rstrip("_")
+
+
+def serialize_model(model: BaseModel) -> str:
+    return "\n".join(
+        [
+            f"  {field}={json.dumps(value, indent=4, default=lambda obj: str(obj))}"
+            for field, value in model.model_dump().items()
+        ]
+    )
