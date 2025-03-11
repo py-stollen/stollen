eb1f678bda959accc7e7776ddb55b5bd2cf601dd
wakaree <svegliatixanax42@gmail.com>
2025-03-11 12:29:55 +0100

    fix: format detailed errors properly

--- stollen/utils/text.py
+++ stollen/utils/text.py
@@ -11,7 +11,7 @@ def camel_to_snake(string: str) -> str:
 def serialize_model(model: BaseModel) -> str:
     return "\n".join(
         [
-            f"  {field}={json.dumps(value, indent=4, default=lambda obj: str(obj))}"
+            f"  {field}={json.dumps(value, indent=4, default=str, ensure_ascii=False)}"
             for field, value in model.model_dump().items()
         ]
     )
